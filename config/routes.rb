1b6591a82d33f585f61a1d5ba803414307990537
Benjamin Galland <galland.benjamin98@gmail.com>
2023-11-28 13:38:05 +0100

    done with some refacto

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,12 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root to: "tasks#index"
-  get "tasks", to: "tasks#index"
-  get "tasks/new", to: "tasks#new"
-  get "tasks/:id", to: "tasks#show", as: "task"
-  get "tasks/:id/edit", to: "tasks#edit", as: "edit"
-  post "tasks", to: "tasks#create"
-  patch "tasks/:id", to: "tasks#update"
-  delete "tasks/:id", to: "tasks#destroy"
+  resources :tasks
+  # get "tasks", to: "tasks#index"
+  # get "tasks/new", to: "tasks#new"
+  # get "tasks/:id", to: "tasks#show", as: "task"
+  # get "tasks/:id/edit", to: "tasks#edit", as: "edit"
+  # post "tasks", to: "tasks#create"
+  # patch "tasks/:id", to: "tasks#update"
+  # delete "tasks/:id", to: "tasks#destroy"
 end
